812a4d0f5f55c413828af4634115628b70d3994a
dbettini <dbettini91@gmail.com>
2016-09-13 21:05:32 +0200

    Switch to a different server
    - also update .gitignore

--- emailServer/index.js
+++ emailServer/index.js
@@ -3,12 +3,10 @@ var email = require("emailjs"),
     config = require("../config");
 
 var server = email.server.connect({
-    host: "mail.vip.hr"
-    //host: "smtp.extensionengine.com",
-    //user: config.email.user,
-    //password: config.email.password,
-    //ssl: true,
-    //port: 465
+    host: "ms1.extensionengine.com",
+    user: config.email.user,
+    password: config.email.password,
+    tls: true
 });
 
 //TODO: PRODUCTION: change 'to'
@@ -16,8 +14,8 @@ server.sendVerificationMail = function (to, token) {
     var url = config.webUrl + "/api/register/" + token;
     server.send({
         from: "noreply@extensionengine.com",
-        //to: to,
-        to: "dbettini@extensionengine.com",
+        to: to,
+        // to: "dbettini@extensionengine.com",
         subject: "Verification for ExtensionEngine Books",
         text: "Please verify your account by clicking on this link: " + url
     }, function (err, message) {
@@ -33,8 +31,8 @@ server.sendUnverifiedBooksMail = function (to) {
     var url = config.webUrl;
     server.send({
         from: "noreply@extensionengine.com",
-        //to: to,
-        to: "dbettini@extensionengine.com",
+        to: to,
+        // to: "dbettini@extensionengine.com",
         subject: "Notification for EE Books admin",
         text: "With great power comes great responsibility! " +
         "There are new books waiting for you to verify them at " + url
@@ -51,7 +49,7 @@ server.sendNewPassMail = function (to, newPass) {
     server.send({
         from: "noreply@extensionengine.com",
         to: to,
-        //to: "dbettini@extensionengine.com",
+        // to: "dbettini@extensionengine.com",
         subject: "New password for EE Books",
         text: "This is your new password: " + newPass
     }, function (err, message) {
