3268cfae2cbf658341b1eb5138c047eb86806d97
Dario Vladovic <d.vladimyr@gmail.com>
2017-06-09 20:15:39 +0200

    Migrating from xo to eslint
    - factoring out linter configuration from package.json
    - introducing eslint config based on xo-space
    - updating package.json metadata
    - removing jshint config

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 2016,
+        sourceType: "module",
+    },
+    env: {
+        es6: true,
+        node: true
+    },
+    extends: "xo-space",
+    rules: {
+        "indent": 0,
+        "no-inline-comments": 0,
+        "no-warning-comments": 0,
+        "quotes": ["error","double"],
+        "curly": ["error","multi-or-nest"],
+        "spaced-comment": ["error", "always",
+            { "markers": ["TODO:", "NOTE:", "FIXME:"] }]
+    }
+};
